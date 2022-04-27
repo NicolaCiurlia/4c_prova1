6f3b15962a18ae3d9b5c53774992779652c06abb
Ciurlia2 <ciurlia@gmail.com>
2022-04-27 11:54:22 +0200

    versione 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,8 +1,14 @@
-
+/**
+ * 
+ * @author Ciurlia Nicola
+ *
+ * @version 2
+ *
+ */
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("prova 2");
 
 	}
 
